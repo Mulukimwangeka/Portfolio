a57ac17511891f5faf445cdc3a0c46bcb48d4a9b
mulukimwangeka <mwangekaruth@gmail.com>
2023-01-22 12:38:52 +0300

    css added in home and about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+// import about.css from styles
+import "../Styles/About.css";
 
 const AboutPage = () => {
 return (
 <div className="container about-page-container">
 <h2>About Me</h2>
-<p>I am a junior developer with a passion for building web applications and learning new technologies. I have experience with HTML, CSS, JavaScript, and React, and I am always eager to expand my skillset and tackle new challenges.</p>
+<p>I am an upcoming developer with a passion for building web applications and learning new technologies. I have experience with HTML, CSS, JavaScript, Ruby,Rails and React, and I am always eager to expand my skillset and tackle new challenges.</p>
 <p>I have a strong foundation in Infomation Technology and a desire to use my skills to create meaningful and impactful projects. I believe that effective communication and teamwork are key to success in the tech industry, and I am committed to working collaboratively and delivering high-quality work.</p>
 <p>In my free time, I enjoy participating in hackathons and coding challenges to stay up-to-date with the latest trends and technologies in the field.</p>
 <h3>Education</h3>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Styles/Home.css";
 
 const HomePage = () => {
 return (
 <div className="container home-page-container">
 <h1>Welcome to My Portfolio</h1>
-<p>I am a junior developer with experience in web development and a passion for learning new technologies. Here are a few of my recent projects:</p>
+<p>I am Ruth Muluki Mwangeka,a junior developer with experience in web development and a passion for learning new technologies. Here are a few of my recent projects and colloborations:</p>
 <div className="row">
 <div className="col-md-4">
 <Link to="/project1">
