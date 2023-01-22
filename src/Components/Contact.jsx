b40fb3b434feffd6eef96cee61b2694bcdd4cf52
mulukimwangeka <mwangekaruth@gmail.com>
2023-01-23 00:07:03 +0300

    Modified all pages

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,9 +9,9 @@ return (
 <h2>Contact Me</h2>
 <p>Thank you for visiting my portfolio. If you have any questions or would like to get in touch, please feel free to contact me through any of the following channels:</p>
 <ul className="md">
-<li>Email: <a className="t-id" href="mailto:mwangekaruth@gmail.com"><FaEnvelope className="icon" /> mwangekaruth@gmail.com</a></li>
-<li>GitHub: <a className="t-id" href="https://github.com/Mulukimwangeka" target="_blank" rel="noopener noreferrer"><FaGithub className="icon" /> https://github.com/Mulukimwangeka</a></li>
-<li>LinkedIn: <a className="t-id" href="https://www.linkedin.com/in/ruth-mwangeka-355b42203/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon" /> https://www.linkedin.com/in/ruth-mwangeka-355b42203/</a></li>
+<li>Email: <a className="a-id" href="mailto:mwangekaruth@gmail.com"><FaEnvelope className="icon" /> mwangekaruth@gmail.com</a></li>
+<li>GitHub: <a className="a-id" href="https://github.com/Mulukimwangeka" target="_blank" rel="noopener noreferrer"><FaGithub className="icon" /> https://github.com/Mulukimwangeka</a></li>
+<li>LinkedIn: <a className="a-id" href="https://www.linkedin.com/in/ruth-mwangeka-355b42203/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon" /> https://www.linkedin.com/in/ruth-mwangeka-355b42203/</a></li>
 </ul>
 </div>
 );
