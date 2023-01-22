6f1ae5ca3095a2f5045f63c57f89d3f568d03667
mulukimwangeka <mwangekaruth@gmail.com>
2023-01-22 13:54:35 +0300

    added fa icons in contact jsx and modfied contact css

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,19 +1,19 @@
-
 import React from "react";
 import "../Styles/Contact.css";
+import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa"; // Import the icons
 
 const ContactPage = () => {
 return (
+
 <div className="container contact-page-container">
 <h2>Contact Me</h2>
 <p>Thank you for visiting my portfolio. If you have any questions or would like to get in touch, please feel free to contact me through any of the following channels:</p>
 <ul>
-<li>Email: <a href="mailto:mwangekaruth@gmail.com">mwangekaruth@gmail.com</a></li>
-<li>GitHub: <a href="https://github.com/Mulukimwangeka" target="_blank" rel="noopener noreferrer">https://github.com/Mulukimwangeka</a></li>
-<li>LinkedIn: <a href="https://www.linkedin.com/in/ruth-mwangeka-355b42203/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/ruth-mwangeka-355b42203/</a></li>
+<li>Email: <a href="mailto:mwangekaruth@gmail.com"><FaEnvelope className="icon" /> mwangekaruth@gmail.com</a></li>
+<li>GitHub: <a href="https://github.com/Mulukimwangeka" target="_blank" rel="noopener noreferrer"><FaGithub className="icon" /> https://github.com/Mulukimwangeka</a></li>
+<li>LinkedIn: <a href="https://www.linkedin.com/in/ruth-mwangeka-355b42203/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon" /> https://www.linkedin.com/in/ruth-mwangeka-355b42203/</a></li>
 </ul>
 </div>
 );
 };
-
 export default ContactPage;
